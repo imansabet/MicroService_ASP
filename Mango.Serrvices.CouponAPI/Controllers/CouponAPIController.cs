b6e1a776ccfee85f5c2f25c012b43949bc9fcaf8
iman <dev.imansabet@gmail.com>
2024-02-13 20:06:41 +0330

    CouponApi : Get Endpoints

--- Mango.Serrvices.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Serrvices.CouponAPI/Controllers/CouponAPIController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Services.CouponAPI.Controllers
@@ -7,5 +9,41 @@ namespace Mango.Services.CouponAPI.Controllers
     [ApiController]
     public class CouponAPIController : ControllerBase
     {
+        private readonly AppDbContext _db;
+
+        public CouponAPIController(AppDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public object Get() 
+        {
+            try 
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList(); 
+                return objList;
+            }
+            catch (Exception e) 
+            {
+                
+            }
+            return null;
+        }
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                Coupon objList = _db.Coupons.First(u=> u.CouponId == id);
+                return objList;
+            }
+            catch (Exception e)
+            {
+
+            }
+            return null;
+        }
+
     }
 }
