dac4d00d7bc9f5b03c4404b1c8fe7fb0e9381776
iman <dev.imansabet@gmail.com>
2024-02-13 18:53:25 +0330

    CouponApi: Port changed :  7001, Add Model : Coupon Add DTO : CouponDTO

---
+++ Mango.Serrvices.CouponAPI/Models/DTO/CouponDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Mango.Serrvices.CouponAPI.Models.DTO
+{
+    public class CouponDTO
+    {
+        
+            public int CouponId { get; set; }
+            public string CouponCode { get; set; }
+            public double DiscountAmount { get; set; }
+            public int MinAmount { get; set; }
+        
+    }
+}
