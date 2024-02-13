48cba047f954d6c622caa6a14e5cc2bbb1295729
iman <dev.imansabet@gmail.com>
2024-02-13 19:23:37 +0330

    CouponAPI : Add DbContext

---
+++ Mango.Serrvices.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Serrvices.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Serrvices.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
--- Mango.Serrvices.CouponAPI/Models/Coupon.cs
+++ Mango.Serrvices.CouponAPI/Models/Coupon.cs
@@ -1,9 +1,14 @@
-﻿namespace Mango.Serrvices.CouponAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Serrvices.CouponAPI.Models
 {
     public class Coupon
     {
+        [Key]
         public int CouponId { get; set; }
+        [Required]
         public string CouponCode { get; set;}
+        [Required]
         public double DiscountAmount { get; set;}
         public int MinAmount { get; set;}
     }
