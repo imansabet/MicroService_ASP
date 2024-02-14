8717e8cc8e6665ce4963df0691f87e62a8dd3fb2
iman <dev.imansabet@gmail.com>
2024-02-14 19:46:44 +0330

    Add Utility : SD.cs Define ApiType - use in RequestDTO

--- Mango.Web/Models/RequestDTO.cs
+++ Mango.Web/Models/RequestDTO.cs
@@ -1,8 +1,10 @@
-﻿namespace Mango.Web.Models
+﻿using static Mango.Web.Utility.SD;
+
+namespace Mango.Web.Models
 {
     public class RequestDTO
     {
-        public string ApiType { get; set; } = "GET";
+        public ApiType ApiType { get; set; } = ApiType.GET;
         public string Url { get; set; }
         public object Data { get; set; }
         public string AccessToken { get; set; }
