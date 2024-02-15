59066bdc578a9d38d6efabc03046bb30cdc0a788
iman <dev.imansabet@gmail.com>
2024-02-15 18:23:25 +0330

    Program.cs : inject Services , config HttpClient

---
+++ Mango.Web/Service/CouponService.cs
@@ -0,0 +1,45 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+
+namespace Mango.Web.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IBaseService _baseService;
+
+        public CouponService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public Task<ResponseDTO?> CreateCouponsAsync(CouponDTO couponDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO?> DeleteCouponsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO?> GetAllCouponAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO?> GetCouponAsync(string couponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO?> GetCouponByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDTO?> UpdateCouponsAsync(CouponDTO couponDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
