bcbfdd97817b02995d402072794c15ceb3e53c81
iman <dev.imansabet@gmail.com>
2024-02-15 22:27:54 +0330

    Add Action:  CouponCreate , add view : CouponCreate

--- Mango.Web/Controllers/CouponController.cs
+++ Mango.Web/Controllers/CouponController.cs
@@ -23,5 +23,9 @@ namespace Mango.Web.Controllers
             }
             return View(list);
         }
+        public async Task<IActionResult> CouponCreate() 
+        {
+            return View();
+        }
     }
 }
