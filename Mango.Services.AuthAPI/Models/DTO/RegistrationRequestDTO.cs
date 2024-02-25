16fce92fb0992dd63a44fd7044edffebadd3bb85
iman <dev.imansabet@gmail.com>
2024-02-25 09:34:38 +0330

    Assign Role

--- Mango.Services.AuthAPI/Models/DTO/RegistrationRequestDTO.cs
+++ Mango.Services.AuthAPI/Models/DTO/RegistrationRequestDTO.cs
@@ -6,5 +6,7 @@
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
         public string password { get; set; }
+        public string? Role { get; set; }
+
     }
 }
