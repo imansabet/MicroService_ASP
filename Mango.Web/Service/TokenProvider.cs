2dd95255623c29670e93e45b64780c2062668dfd
iman <dev.imansabet@gmail.com>
2024-02-27 20:19:39 +0330

    Token Provider Service

---
+++ Mango.Web/Service/TokenProvider.cs
@@ -0,0 +1,34 @@
+﻿using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+using Newtonsoft.Json.Linq;
+
+namespace Mango.Web.Service
+{
+    public class TokenProvider : ITokenProvider
+    {
+        private readonly IHttpContextAccessor _ContextAccessor;
+
+        public TokenProvider(IHttpContextAccessor ContextAccessor)
+        {
+            _ContextAccessor = ContextAccessor;
+        }
+        public void ClearToken()
+        {
+            _ContextAccessor.HttpContext?.Response.Cookies.Delete(SD.TokenCookie);
+
+        }
+
+        public string? GetToken()
+        {
+            string? token = null;
+            bool? hasToken = _ContextAccessor.HttpContext?.Request.Cookies.TryGetValue(SD.TokenCookie, out token); 
+            return hasToken is true ? token : null;
+
+        }
+
+        public void SetToken(string token)
+        {
+            _ContextAccessor.HttpContext?.Response.Cookies.Append(SD.TokenCookie, token);
+        }
+    }
+}
