9756b780d0ce4f60230c3daecb7471ffd9fde7cf
iman <dev.imansabet@gmail.com>
2024-03-22 17:13:02 +0330

    Create Models And DTO`s

---
+++ Mango.Services.ShoppingCartAPI/Models/DTO/CartDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Mango.Services.ShoppingCartAPI.Models.DTO
+{
+    public class CartDTO
+    {
+        public CartHeaderDTO CartHeader { get; set; }
+        public IEnumerable<CartDetialsDTO>? CartDetails { get; set; }
+    }
+}
+
---
+++ Mango.Services.ShoppingCartAPI/Models/DTO/CartDetialsDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Mango.Services.ShoppingCartAPI.Models.DTO
+{
+    public class CartDetialsDTO
+    {
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        public CartHeaderDto? CartHeader { get; set; }
+        public int ProductId { get; set; }
+        public ProductDto? Product { get; set; }
+        public int Count { get; set; }
+    }
+}
