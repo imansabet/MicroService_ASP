e67d667d8fcf6e0cd5979112ba6d1002af3383ea
iman <dev.imansabet@gmail.com>
2024-03-22 17:25:22 +0330

    create APIController

---
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Models;
+using Mango.Services.ShoppingCartAPI.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ShoppingCartAPI.Controllers
+{
+    [Route("api/cart")]
+    [ApiController]
+    public class CartAPIController : ControllerBase
+    {
+        private ResponseDTO _response;
+        private IMapper _mapper;
+        private readonly AppDbContext _db;
+        public CartAPIController(AppDbContext db,
+            IMapper mapper)
+        {
+            _db = db;
+            _response = new ResponseDTO();
+            _mapper = mapper;
+        }
+    }
+}
