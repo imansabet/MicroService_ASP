a79fc664da4c53f883c1a28b4e1b8ec98b6af037
iman <dev.imansabet@gmail.com>
2024-03-22 19:07:13 +0330

    GetCart Endpoint(+ InterApi call)

--- Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Mango.Services.ShoppingCartAPI.Models;
 using Mango.Services.ShoppingCartAPI.Models.DTO;
+using Mango.Services.ShoppingCartAPI.Service.IService;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,12 +15,16 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
         private ResponseDTO _response;
         private IMapper _mapper;
         private readonly AppDbContext _db;
-        public CartAPIController(AppDbContext db,
+        private IProductService _productService;
+        private ICouponService _couponService;
+        public CartAPIController(AppDbContext db, IProductService productService,ICouponService couponService,
             IMapper mapper)
         {
             _db = db;
             _response = new ResponseDTO();
             _mapper = mapper;
+            _productService = productService;
+            _couponService = couponService;
         }
         [HttpPost("CartUpsert")]
         public async Task<ResponseDTO> CartUpsert(CartDTO cartDto)
@@ -96,6 +101,46 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
             {
                 _response.Message = ex.Message.ToString();
                 _response.IsSuccess = false;
+            }
+            return _response;
+        }
+        [HttpGet("GetCart/{userId}")]
+        public async Task<ResponseDTO> GetCart(string userId)
+        {
+            try
+            {
+                CartDTO cart = new()
+                {
+                    CartHeader = _mapper.Map<CartHeaderDTO>(_db.CartHeaders.First(u => u.UserId == userId))
+                };
+                cart.CartDetails = _mapper.Map<IEnumerable<CartDetialsDTO>>(_db.CartDetails
+                    .Where(u => u.CartHeaderId == cart.CartHeader.CartHeaderId));
+
+                IEnumerable<ProductDTO> productDtos = await _productService.GetProducts();
+
+                foreach (var item in cart.CartDetails)
+                {
+                    item.Product = productDtos.FirstOrDefault(u => u.ProductId == item.ProductId);
+                    cart.CartHeader.CartTotal += (item.Count * item.Product.Price);
+                }
+
+                //apply coupon if any
+                if (!string.IsNullOrEmpty(cart.CartHeader.CouponCode))
+                {
+                    CouponDTO coupon = await _couponService.GetCoupon(cart.CartHeader.CouponCode);
+                    if (coupon != null && cart.CartHeader.CartTotal > coupon.MinAmount)
+                    {
+                        cart.CartHeader.CartTotal -= coupon.DiscountAmount;
+                        cart.CartHeader.Discount = coupon.DiscountAmount;
+                    }
+                }
+
+                _response.Result = cart;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
             }
             return _response;
         }
---
+++ Mango.Services.ShoppingCartAPI/Service/CouponService.cs
@@ -0,0 +1,29 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.DTO;
+using Mango.Services.ShoppingCartAPI.Service.IService;
+using Newtonsoft.Json;
+
+namespace Mango.Services.ShoppingCartAPI.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CouponService(IHttpClientFactory clientFactory)
+        {
+            _httpClientFactory = clientFactory;
+        }
+
+        public async Task<CouponDTO> GetCoupon(string couponCode)
+        {
+            var client = _httpClientFactory.CreateClient("Coupon");
+            var response = await client.GetAsync($"/api/coupon/GetByCode/{couponCode}");
+            var apiContet = await response.Content.ReadAsStringAsync();
+            var resp = JsonConvert.DeserializeObject<ResponseDTO>(apiContet);
+            if (resp != null && resp.IsSuccess)
+            {
+                return JsonConvert.DeserializeObject<CouponDTO>(Convert.ToString(resp.Result));
+            }
+            return new CouponDTO();
+        }
+    }
+}
